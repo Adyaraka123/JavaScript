a943cd1facf0c01961f2ee30f4f66ae51c83f8f3
Adyaraka123 <144977802+Adyaraka123@users.noreply.github.com>
2025-01-14 13:50:08 +0700

    secret class

---
+++ meeting6baru/script.js
@@ -0,0 +1,28 @@
+let number1 = 10;
+let number2 = 12;
+let number3 = '10';
+let number4 = 'alpukat';
+let number5 = 'Alpukat';
+let number6 = 'cherry';
+let number7 = 10;
+let number8;
+let number9 = null;
+// KEGUNAAN >, contoh = 2 > 1 ,  berarti 2 lebih besar dari 1. 
+// KEGUNAAN <, contoh = 2 < 1 ,berarti 2 lebih kecil dari 1 .
+// NAMUN KITA JANGAN TERTIPU OLEH VARIABLE NYA, KITA LIHAT VALUE NYA .
+// KEGUNAAN >= , contoh 2 >= 3 , berarti 2 itu sama seperti 3,liat lagi value nya, kalau sama berarti true, kalo beda berartifalse.
+// KEGUNAAN ==, contoh 1 == 3, true, karena tipe data nya harus beda.
+// KEGUNAAN ===, contoh 1 === 3, false,karena tipe datanya beda.
+// KEGUNAAN !=, contoh 1 != 3, false karena tipe valuenya berbeda
+// KEGUNAAN !==, contoh 1 !==, true, karena tipe datanya harus beda
+// PERBANDING STRING, contoh Apple > Banana , false, karena huruf a itu lebih kecil dari b. namun kalau banana > apple, true, karena huruf b lebih besar dari a
+// KALAU KITA MENGGUNAKAN huruf A , maka dia lebih kecil dari huruf a
+console.log(number8)
+console.log(number2 < number1)
+console.log(number2 > number1)
+console.log(number1 == number3)
+console.log(number1 === number3)
+console.log(number1 != number3)
+console.log(number1 !== number3)
+// BERDASARKAN HURUF APLHABET ↓
+console.log(number4 > number6)
