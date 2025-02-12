610b8d241a5290fc42a63e512471fe4691275e20
Adyaraka123 <144977802+Adyaraka123@users.noreply.github.com>
2025-02-12 20:13:15 +0700

    tugas14

---
+++ meeting11dirumah/tugas14.js
@@ -0,0 +1,11 @@
+function calculateArea(length, width) {
+  return length * width;
+}
+
+//Expression Function
+const calculateArea = function (length, width) {
+  return length * width;
+};
+
+//Arrow Function
+const calculateArea = (length, width) => length * width;
