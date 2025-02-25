1bd39abd653c6e612121832ea556b158818a3ee0
Adyaraka123 <144977802+Adyaraka123@users.noreply.github.com>
2025-02-25 13:40:19 +0700

    reall

---
+++ meeting14,SpesialJavaScript/tugas17SpesialJavaScript.js
@@ -0,0 +1,39 @@
+// Tugas 17 Spesial Java Script
+
+// Nomer 1
+
+console.log(umur)
+var umur = 25;
+console.log(umur);
+
+// Nomer 2
+
+
+
+//  Nomer 3
+
+let data = "halo";
+console.log(typeof data)
+
+data = 42;
+console.log(typeof data )
+
+data = true;
+console.log(typeof data)
+
+// Nomer 4
+
+
+
+
+// Nomer 5
+//  Confirm
+
+
+
+let x = 10;
+if (x == "10") {
+    console.log("True");
+} else {
+    console.log("False");
+}
